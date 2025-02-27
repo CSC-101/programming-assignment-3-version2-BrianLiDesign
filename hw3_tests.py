38197f4657faf223731b1155c65fca8497851b93
BrianLi808 <brian.li.social@gmail.com>
2025-02-26 23:30:43 -0800

    Completion of hw3.py and hw3_tests.py

--- hw3_tests.py
+++ hw3_tests.py
@@ -2,6 +2,11 @@ import data
 import build_data
 import unittest
 
+from hw3 import (population_total, filter_by_state, population_by_education,
+                 population_by_ethnicity, population_below_poverty_level, percent_by_education,
+                 percent_by_ethnicity,percent_below_poverty_level, education_greater_than,
+                 education_less_than, ethnicity_less_than, ethnicity_greater_than,
+                 below_poverty_level_greater_than, below_poverty_level_less_than)
 
 # These two values are defined to support testing below. The
 # data within these structures should not be modified. Doing
@@ -179,28 +184,198 @@ class TestCases(unittest.TestCase):
     pass
 
     # Part 1
-    # test population_total
+    def test_population_total_standard(self):
+        result = population_total(reduced_data)
+        self.assertEqual(result, 655813)
+
+    def test_population_total_some_counties(self):
+        counties = [
+            reduced_data[0],
+            reduced_data[2],
+            reduced_data[4]
+        ]
+        result = population_total(counties)
+        self.assertEqual(result, 337100)
 
     # Part 2
-    # test filter_by_state
+    def test_filter_by_state_standard(self):
+        ca_counties = filter_by_state(reduced_data, "CA")
+        self.assertEqual(len(ca_counties), 2)
+        county_names = {county.county for county in ca_counties}
+        expected = {"San Luis Obispo County", "Yolo County"}
+        self.assertEqual(county_names, expected)
+
+    def test_filter_by_state_empty(self):
+        result = filter_by_state(reduced_data, "XX")
+        self.assertEqual(result, [])
 
     # Part 3
-    # test population_by_education
-    # test population_by_ethnicity
-    # test population_below_poverty_level
+    def test_population_by_education_standard(self):
+        result = population_by_education([reduced_data[0], reduced_data[1], reduced_data[2], reduced_data[3],
+                                          reduced_data[4], reduced_data[5], reduced_data[6]],
+                                         "Bachelor's Degree or Higher")
+        expected = (
+            float(55395 * 0.209) + # Autauga County
+            float(61697 * 0.143) + # Crawford County
+            float(279083 * 0.315) + # San Luis Obispo County
+            float(207590 * 0.379) + # Yolo county
+            float(2622 * 0.179) + # Butte County
+            float(42225 * 0.152) + # Pettis County
+            float(7201 * 0.172) # Weston County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_population_by_education_some_counties(self):
+        result = population_by_education([reduced_data[0], reduced_data[1]], "High School or Higher")
+        expected = (
+            float(55395 * 0.856) + # Autauga County
+            float(61697 * 0.822) # Crawford County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_population_by_ethnicity(self):
+        result = population_by_ethnicity(reduced_data, "Asian Alone")
+        expected = (
+            float(55395 * 0.011) +  # Autauga County
+            float(61697 * 0.016) +  # Crawford County
+            float(279083 * 0.038) +  # San Luis Obispo County
+            float(207590 * 0.138) +  # Yolo County
+            float(2622 * 0.003) +  # Butte County
+            float(42225 * 0.007) +  # Pettis County
+            float(7201 * 0.004)  # Weston County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_population_by_ethnicity_some_counties(self):
+        result = population_by_ethnicity([reduced_data[2], reduced_data[3]], "Black Alone")
+        expected = (
+            float(279083 * 0.022) +  # San Luis Obispo County
+            float(207590 * 0.03)  # Yolo County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_population_below_poverty_level_standard(self):
+        result = population_below_poverty_level(reduced_data)
+        expected = (
+            float(55395 * 0.121) +  # Autauga County
+            float(61697 * 0.202) +  # Crawford County
+            float(279083 * 0.143) +  # San Luis Obispo County
+            float(207590 * 0.191) +  # Yolo County
+            float(2622 * 0.157) +  # Butte County
+            float(42225 * 0.184) +  # Pettis County
+            float(7201 * 0.112)  # Weston County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_population_below_poverty_level_some_counties(self):
+        result = population_below_poverty_level([reduced_data[4], reduced_data[5], reduced_data[6]])
+        expected = (
+            float(2622 * 0.157) +  # Butte County
+            float(42225 * 0.184) +  # Pettis County
+            float(7201 * 0.112)  # Weston County
+        )
+        self.assertAlmostEqual(result, expected, places=5)
 
     # Part 4
-    # test percent_by_education
-    # test percent_by_ethnicity
-    # test percent_below_poverty_level
+    def test_percent_by_education_standard(self):
+        counties = reduced_data
+        education_key = "Bachelor's Degree or Higher"
+        result = percent_by_education(counties, education_key)
+        expected_total_population = 55395 + 61697 + 279083 + 207590 + 2622 + 42225 + 7201
+        expected_total_educated = (
+            float(55395 * 0.209) + # Autauga County
+            float(61697 * 0.143) + # Crawford County
+            float(279083 * 0.315) + # San Luis Obispo County
+            float(207590 * 0.379) + # Yolo County
+            float(2622 * 0.179) + # Butte County
+            float(42225 * 0.152) + # Pettis County
+            float(7201 * 0.172) # Weston County
+        )
+        expected = expected_total_educated / expected_total_population * 100
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_percent_by_education_some_counties(self):
+        counties = [reduced_data[0], reduced_data[2], reduced_data[4]]
+        education_key = "Bachelor's Degree or Higher"
+        result = percent_by_education(counties, education_key)
+        expected_total_population = 55395 + 279083 + 2622
+        expected_total_educated = (
+            float(55395 * 0.209) + # Autauga County
+            float(279083 * 0.315) + # San Luis Obispo County
+            float(2622 * 0.179) # Butte County
+        )
+        expected = expected_total_educated / expected_total_population * 100
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_percent_by_ethnicities_standard(self):
+        counties = reduced_data
+        education_key = "Asian Alone"
+        result = percent_by_ethnicity(counties, education_key)
+        expected_total_population = 55395 + 61697 + 279083 + 207590 + 2622 + 42225 + 7201
+        expected_total_educated = (
+            float(55395 * 0.011) +  # Autauga County
+            float(61697 * 0.016) +  # Crawford County
+            float(279083 * 0.038) +  # San Luis Obispo County
+            float(207590 * 0.138) +  # Yolo County
+            float(2622 * 0.003) +  # Butte County
+            float(42225 * 0.007) +  # Pettis County
+            float(7201 * 0.004)  # Weston County
+        )
+        expected = expected_total_educated / expected_total_population * 100
+        self.assertAlmostEqual(result, expected, places=5)
+
+    def test_percent_below_poverty_level_standard(self):
+        counties = reduced_data
+        result = percent_below_poverty_level(counties)
+        expected_total_population = 55395 + 61697 + 279083 + 207590 + 2622 + 42225 + 7201
+        expected_total_impoverished = (
+                float(55395 * 0.121) +  # Autauga County
+                float(61697 * 0.202) +  # Crawford County
+                float(279083 * 0.143) +  # San Luis Obispo County
+                float(207590 * 0.191) +  # Yolo County
+                float(2622 * 0.157) +  # Butte County
+                float(42225 * 0.184) +  # Pettis County
+                float(7201 * 0.112)  # Weston County
+        )
+        expected = expected_total_impoverished / expected_total_population * 100
+        self.assertAlmostEqual(result, expected, places=5)
 
     # Part 5
-    # test education_greater_than
-    # test education_less_than
-    # test ethnicity_greater_than
-    # test ethnicity_less_than
-    # test below_poverty_level_greater_than
-    # test below_poverty_level_less_than
+    def test_education_greater_than_standard(self):
+        counties = reduced_data
+        result = education_greater_than(counties, "Bachelor's Degree or Higher", 30.0)
+        expected = [reduced_data[2], reduced_data[3]]
+        self.assertEqual(result, expected)
+
+    def test_education_less_than_standard(self):
+        counties = reduced_data
+        result = education_less_than(counties, "Bachelor's Degree or Higher", 15.0)
+        expected = [reduced_data[1]]
+        self.assertEqual(result, expected)
+
+    def test_ethnicity_greater_than_standard(self):
+        counties = reduced_data
+        result = ethnicity_greater_than(counties, "Asian Alone", 10.0)
+        expected = [reduced_data[3]]
+        self.assertEqual(result, expected)
+
+    def test_ethnicity_less_than_standard(self):
+        counties = reduced_data
+        result = ethnicity_less_than(counties, "Asian Alone", 1.0)
+        expected = [reduced_data[4], reduced_data[5], reduced_data[6]]
+        self.assertEqual(result, expected)
+
+    def test_percent_below_poverty_level_greater_than_standard(self):
+        counties = reduced_data
+        result = below_poverty_level_greater_than(counties, 20.0)
+        expected = [reduced_data[1]]
+        self.assertEqual(result, expected)
+
+    def test_percent_below_poverty_level_less_than_standard(self):
+        counties = reduced_data
+        result = below_poverty_level_less_than(counties, 15.0)
+        expected = [reduced_data[0], reduced_data[2], reduced_data[6]]
+        self.assertEqual(result, expected)
 
 
 
